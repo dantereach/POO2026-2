955c8ee11b24fc9be296ec96c3cefe6db0696fd2
dantereach <danterichito@gmaiil.com>
2026-01-20 11:07:17 -0600

    act2

--- java/src/src2/edu/miguel/gonzalez/actividad0/Main.java
+++
@@ -1,9 +0,0 @@
-package src2.edu.miguel.gonzalez.actividad0;
-import edu.miguel.gonzalez.actividad0.ui.CLI;
-
-public class Main {
-    public static void main(String[] args){
-        CLI.start();
-    }
-
-}
